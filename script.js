44d80c1f87a8d929bea5dc29767751a835c9a817
VelmaFebreze <velmafebreze99@gmail.com>
2023-10-20 11:42:04 -0700

    testing

--- script.js
+++ script.js
@@ -3,30 +3,41 @@ $(document).ready(function () {
     $('#submit').prop('disabled', true);
 
     $('input[type="radio"]').click(function () {
-        // Disable all radio buttons within the same question
-        var questionName = $(this).attr('name');
-        $('input[name="' + questionName + '"]').attr('disabled', true);
-
         // Enable the submit button
         $('#submit').prop('disabled', false);
 
         // Determine if the selected answer is correct
         var isCorrect = $(this).val() === 'correct';
 
+        // Remove background colors from all options in the same question
+        var $options = $(this).closest('.question').find('.options label');
+        $options.removeClass('correct-option incorrect-option');
+
         // Highlight the correct options green and all others red
-        $(this).closest('.question').find('.options label').addClass('incorrect-option');
-        $(this).closest('.question').find('input[value="correct"]').parent().removeClass('incorrect-option').addClass('correct-option');
+        $options.filter('input[value="correct"]').addClass('correct-option');
+        $options.filter(':not(:has(input[value="correct"]))').addClass('incorrect-option');
 
         // Set feedback based on correctness
         var feedback = isCorrect ? 'Right!' : 'Wrong.';
         var feedbackClass = isCorrect ? 'correct-feedback' : 'incorrect-feedback';
-        
+
         $(this).closest('.question').find('.feedback').text(feedback).addClass(feedbackClass);
     });
 
     $('#submit').click(function () {
-        // You can add any additional functionality when the user submits the quiz here
-        // For example, you can calculate and display the total score
-        // This is where you can show the total score or perform other actions
+        // Calculate the score
+        var totalQuestions = $('.question').length;
+        var correctAnswers = $('.correct-option').length;
+
+        // Handle the case when all answers are incorrect
+        if (totalQuestions === 0) {
+            var scoreMessage = 'Your score: 0%';
+        } else {
+            var score = (correctAnswers / totalQuestions) * 100;
+            var scoreMessage = 'Your score: ' + score.toFixed(2) + '%';
+        }
+
+        // Display the score to the user
+        $('.feedback').text(scoreMessage);
     });
 });
